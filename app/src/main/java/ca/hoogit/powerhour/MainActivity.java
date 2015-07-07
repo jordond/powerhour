ff5437d982906aced762808b7fc6e8bdf09dff73
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-07-07 15:57:11 -0400

    Getting sorted out

--- app/src/main/java/ca/hoogit/powerhour/MainActivity.java
+++ app/src/main/java/ca/hoogit/powerhour/MainActivity.java
@@ -2,16 +2,25 @@ package ca.hoogit.powerhour;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    @Bind(R.id.appBar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
     }
 
     @Override
