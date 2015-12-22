e4dd9bcb629278d66f88d5c8fbdf9b5f30ab166c
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-12-22 14:44:08 -0500

    Started on data listener
    Created a common library to share classes between modules

---
+++ wear/src/main/java/ca/hoogit/powerhour/GameListenerService.java
@@ -0,0 +1,12 @@
+package ca.hoogit.powerhour;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class GameListenerService extends WearableListenerService {
+    private static final String TAG = GameListenerService.class.getSimpleName();
+
+}
